0e08b46332271e8bb676b96884e64385427abe71
SANDHRAMARIYA <sandhramariya09@gmail.com>
2022-10-15 11:08:06 +0530

    serve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewAllRailsComponent } from './view-all-rails/view-all-rails.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 const appRoutes:Routes=[
   {
     path:"",component:ViewAllRailsComponent
@@ -22,7 +23,9 @@ const appRoutes:Routes=[
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/view-all-rails/view-all-rails.component.ts
+++ src/app/view-all-rails/view-all-rails.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-view-all-rails',
@@ -7,8 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewAllRailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myapi:ApiService) { 
+    this.fetchData()
+  }
 
+
+
+  fetchData=()=>{
+    this.myapi.viewTrains().subscribe(
+      (data)=>{
+        this.trainsData=data
+      }
+    )
+  }
   
 trainsData:any={}
   ngOnInit(): void {
