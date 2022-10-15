b06dc3e32f0f7cd864503d9b17ea58319c9e83fc
SANDHRAMARIYA <sandhramariya09@gmail.com>
2022-10-15 10:49:29 +0530

    viewall

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewAllRailsComponent } from './view-all-rails/view-all-rails.component';
+
+const appRoutes:Routes=[
+  {
+    path:"",component:ViewAllRailsComponent
+  }
+]
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewAllRailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/view-all-rails/view-all-rails.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all-rails',
+  templateUrl: './view-all-rails.component.html',
+  styleUrls: ['./view-all-rails.component.css']
+})
+export class ViewAllRailsComponent implements OnInit {
+
+  constructor() { }
+
+  
+trainsData:any={}
+  ngOnInit(): void {
+  }
+
+}
